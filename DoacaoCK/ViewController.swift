f28a2e6ee59e76f4acaf7978b4ba6fd7081b69f7
alcidesjunior <alcidesjunior.infor@gmail.com>
2019-01-29 08:13:13 -0300

    startando o app

---
+++ DoacaoCK/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DoacaoCK
+//
+//  Created by Alcides Junior on 29/01/19.
+//  Copyright © 2019 Alcides Junior. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
