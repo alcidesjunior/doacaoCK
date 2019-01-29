0a0d4db914fba81a9bb24cde8824af82d98a3c7b
alcidesjunior <alcidesjunior.infor@gmail.com>
2019-01-29 08:35:35 -0300

    parte do front feito

--- DoacaoCK/ViewController.swift
+++ DoacaoCK/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var inputPesquisar: UITextField!
+    @IBOutlet weak var imagePreview: UIImageView!
+    @IBOutlet weak var inputNome: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
-
+    @IBAction func buscarButton(_ sender: Any) {
+    }
+    
+    @IBAction func adicionarButton(_ sender: Any) {
+    }
 }
-
+extension ViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! TableViewCell
+        
+        return cell
+    }
+    
+    
+}
