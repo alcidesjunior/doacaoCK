d38249609bb8246995fabc82df6a4cb9e00aa685
jbatistaromao <j.batista.romao@gmail.com>
2019-01-29 11:21:54 -0300

    Save image

---
+++ DoacaoCK/CloudKitManager.swift
@@ -0,0 +1,52 @@
+//
+//  CloudKitManager.swift
+//  DoacaoCK
+//
+//  Created by Matheus Costa on 29/01/19.
+//  Copyright © 2019 Alcides Junior. All rights reserved.
+//
+
+import UIKit
+import CloudKit
+
+class CloudKitManager: NSObject {
+    
+    private let container = CKContainer.default()
+    
+    func search(using predicate: NSPredicate, then completion: @escaping (([CKRecord]?) -> Void)) {
+        var records = [CKRecord]()
+        let query = CKQuery(recordType: "Doacao", predicate: predicate)
+        let operation = CKQueryOperation(query: query)
+        operation.desiredKeys = ["nome"]
+        
+        operation.recordFetchedBlock = { record in
+            records.append(record)
+        }
+        
+        operation.queryCompletionBlock = { (cursor, error) in
+            guard error == nil else {
+                print(error)
+                completion(nil)
+                return
+            }
+            
+            completion(records)
+        }
+        
+        self.container.privateCloudDatabase.add(operation)
+    }
+    
+    func save(record: CKRecord, then completion: @escaping ((CKRecord?) -> Void)) {
+        self.container.privateCloudDatabase.save(record) { (record, error) in
+            guard error == nil else {
+                print(error!)
+                completion(nil)
+                return
+            }
+            
+            print("Record saved")
+            completion(record)
+        }
+    }
+    
+}
---
+++ DoacaoCK/ViewController.swift
@@ -0,0 +1,119 @@
+//
+//  ViewController.swift
+//  DoacaoCK
+//
+//  Created by Alcides Junior on 29/01/19.
+//  Copyright © 2019 Alcides Junior. All rights reserved.
+//
+
+import UIKit
+import CloudKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var inputPesquisar: UITextField!
+    @IBOutlet weak var imagePreview: UIImageView!
+    
+    @IBOutlet weak var inputNome: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
+    let manager = CloudKitManager()
+    var doacoes = [CKRecord]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        self.manager.search(using: NSPredicate(value: true)) { (records) in
+            guard let records = records else { return }
+            
+            DispatchQueue.main.async {
+                self.doacoes = records
+                
+                self.tableView.reloadData()
+            }
+        }
+        
+        inputNome.delegate = self
+        inputPesquisar.delegate = self
+
+    }
+    
+    func QjueryDataBase(search: String)  {
+        
+        if search == ""{
+            self.manager.search(using: NSPredicate(value: true)) { (records) in
+                guard let records = records else { return }
+                
+                DispatchQueue.main.async {
+                    self.doacoes = records
+                    
+                    self.tableView.reloadData()
+                }
+            }
+        }
+        else{
+            let user = inputPesquisar.text
+            self.manager.search(using: NSPredicate(format: "nome == %@", user!)) { (records) in
+                guard let records = records else { return }
+                DispatchQueue.main.async {
+                    self.doacoes = records
+                    
+                    self.tableView.reloadData()
+                }
+            
+        }
+    }
+    }
+    
+    @IBAction func buscarButton(_ sender: Any) {
+        self.view.endEditing(true)
+        QjueryDataBase(search: inputPesquisar.text!)
+    }
+    
+    @IBAction func adicionarButton(_ sender: Any) {
+        let record = CKRecord(recordType: "Doacao")
+        record["nome"] = self.inputNome.text
+        
+        self.manager.save(record: record) { (record) in
+            // Aqui era pra ser tratado o erro.
+            guard let record = record else { return }
+            
+            DispatchQueue.main.async {
+                self.doacoes.append(record)
+                self.tableView.reloadData()
+                self.view.endEditing(true)
+            }
+        }
+         QjueryDataBase(search: inputPesquisar.text!)
+    }
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.doacoes.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! TableViewCell
+        
+        cell.nomeLabel?.text = self.doacoes[indexPath.row].value(forKey: "nome") as? String
+        
+        return cell
+    }
+    
+}
+extension ViewController: UITextFieldDelegate{
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+       textField.resignFirstResponder()
+       
+        return true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        inputPesquisar.resignFirstResponder()
+        inputNome.resignFirstResponder()
+        self.view.endEditing(true)
+    }
+}
